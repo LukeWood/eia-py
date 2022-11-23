7d041fe8b3702460fc13dbee8278ba1bec356c0a
Luke Wood <lukewoodcs@gmail.com>
2022-11-23 15:42:28 -0800

    Update base dir

--- source/conf.py
+++ source/conf.py
@@ -25,6 +25,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_baseurl = '/docs/'
+
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 extensions = ['sphinx.ext.napoleon',
